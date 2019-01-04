e1cd7fa347c10590403963ff1b7e4b7955f6d1e1
mohamed mahmoud <mshaat@10degrees.io>
2019-01-05 00:44:01 +0200

    Initial Commit

---
+++ LanguageChange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LanguageChange
+//
+//  Created by LapStore on 1/5/19.
+//  Copyright © 2019 shaat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
