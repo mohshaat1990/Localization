db3c164df1dfc97cfbf6be3160220d2a0637d2d0
LapStore <mshaat@10degrees.io>
2019-01-06 20:24:31 +0200

    adding first version that support localize storyBoard

--- LanguageChange/ViewController.swift
+++ LanguageChange/ViewController.swift
@@ -2,19 +2,31 @@
 //  ViewController.swift
 //  LanguageChange
 //
-//  Created by LapStore on 1/5/19.
-//  Copyright © 2019 shaat. All rights reserved.
+//  Created by MOHAMED MAHMOUD on 1/5/19.
+//  Copyright © 2019 MOHAMED MAHMOUD. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    var shaat = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func changeLanguageAction(_ sender: Any) {
+        if MoLocalization.currentAppleLanguage() == Constants.arabicLanguage {
+            MoLocalization.setCurrentLang(lang: Constants.defaultLanguage,
+                                          isRightToLeft: false,
+                                          forceReset: true,
+                                          startStoryBorad: "Main",
+                                          startViewController: "ViewController")
+        } else {
+             MoLocalization.setCurrentLang(lang: Constants.arabicLanguage,
+                                           isRightToLeft: true,
+                                           forceReset: true,
+                                           startStoryBorad: "Main",
+                                           startViewController: "ViewController")
+        }
+    }
 }
-
---
+++ Pods/Target Support Files/Pods-LanguageChange/Pods-LanguageChange-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LanguageChange : NSObject
+@end
+@implementation PodsDummy_Pods_LanguageChange
+@end
